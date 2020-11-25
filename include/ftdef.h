5efab0329b2f5bcfb4cff43b574d7f1063b7dfbe
Krutix <pigeon.krutix@gmail.com>
2020-11-25 19:23:49 +0300

    collections -> data_structures

--- include/ftdef.h
+++ include/ftdef.h
@@ -6,7 +6,7 @@
 /*   By: krutix <krutix@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/01 20:40:51 by krutix            #+#    #+#             */
-/*   Updated: 2020/11/22 22:29:02 by krutix           ###   ########.fr       */
+/*   Updated: 2020/11/25 19:22:27 by krutix           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 #include <stddef.h>
 #include <limits.h>
+#include <stdint.h>
 
 typedef enum e_bool { t_false = 0, t_true = 1 } t_bool;
 
@@ -27,9 +28,14 @@ typedef short int				sint;
 typedef unsigned short int		usint;
 typedef unsigned char			uchar;
 
+/*
+**	general data type for data structure
+*/
 typedef union	u_data {
 	void		*p;
-	llint		i;
+	char		*s;
+	intptr_t	i;
+	uintptr_t	u;
 }				t_data;
 
 
