6c327d35d3825c491efc88569e0ab2d648d0f61d
Krutix <pigeon.krutix@gmail.com>
2021-07-26 00:45:06 +0300

    [vector] fix copy constructor

--- data_structure/vector/ft_vec_copy.c
+++ data_structure/vector/ft_vec_copy.c
@@ -1,4 +1,5 @@
 #include "ft_vector.h"
+#include "ft_string.h"
 
 t_bool	ft_vec_copy_construct(t_vector *copy, t_vector *sample)
 {
@@ -6,6 +7,9 @@ t_bool	ft_vec_copy_construct(t_vector *copy, t_vector *sample)
 	copy->capacity = sample->size;
 	copy->cell_size = sample->cell_size;
 	copy->array = malloc(sample->size * sample->cell_size);
+	if (!copy->array)
+		return (t_false);
 	ft_memcpy(copy->array, sample->array, \
 		sample->size * sample->cell_size);
+	return (t_true);
 }
