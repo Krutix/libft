bf2fc7319f56fab57198d130ee65abeb30ac41da
Carmine Watcher <CWatcher@student.21-school.r>
2021-09-10 08:11:07 +0300

    libft.mk: release->debug

--- libft.mk
+++ libft.mk
@@ -1,8 +1,8 @@
 LIBFT_DIR ?= libft/
-LIBFT_BUILD_DIR = .release/
+LIBFT_BUILD_DIR = .debug/
 LIBFT_INC = ${realpath ${LIBFT_DIR}/include/}
 LIBFT_LIB_DIR = ${realpath ${LIBFT_DIR}${LIBFT_BUILD_DIR}}
 LIBFT_LIB = -lft
 
 lft/%:
-	make -C ${LIBFT_DIR} rl/${@:lft/%=%}
+	make -C ${LIBFT_DIR} db/${@:lft/%=%}
