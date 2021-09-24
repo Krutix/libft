b515155a261b65d4865cba059030c7158082b43d
Krutix <pigeon.krutix@gmail.com>
2021-09-25 02:48:30 +0300

    [error] update error name, not it is ftE

--- include/ft_error.h
+++ include/ft_error.h
@@ -1,14 +1,16 @@
 #ifndef FT_ERROR_H
 # define FT_ERROR_H
 
-typedef int t_error;
+typedef int t_ftE;
 
 enum e_error
 {
-	error_no_error = 0,
-	error_allocation_fail,
-	error_parse_error,
-	error_user_define_error = (1 << 15)
+	ftE_ok = 0,
+	ftE_fail,
+	ftE_bad_alloc,
+	ftE_bad_arg,
+	ftE_parse_error,
+	ftE_user_define_error_namespace = (1 << 15)
 };
 
 #endif
