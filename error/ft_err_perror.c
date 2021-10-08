88d20bd6adc5376b8ee8316c6670856a0b3d18e8
Function Diego <fdiego@oa-j1.msk.21-school.ru>
2021-10-09 01:14:28 +0300

    [error] ft_perror and ft_strerr

--- error/ft_err_perror.c
+++ error/ft_err_perror.c
@@ -10,11 +10,33 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include <unistd.h>
 #include <ft_error.h>
+#include <ft_io.h>
+
+char const	*ft_strerr(t_ft_err err)
+{
+	return ((char const*)(0 \
+		+ (err == ft_err_ok) * (intptr_t)"ok" \
+		+ (err == ft_err_fail) * (intptr_t)"fail" \
+		+ (err == ft_err_bad_arg) * (intptr_t)"bad argument" \
+		+ (err == ft_err_toomanyargs) * (intptr_t)"too many arguments" \
+		+ (err == ft_err_bad_alloc) * (intptr_t)"memory allocation failed" \
+		+ (err == ft_err_overflow) * (intptr_t)"overflow" \
+		+ (err == ft_err_underflow) * (intptr_t)"underflow" \
+		+ (err == ft_err_unclosedquotes) * (intptr_t)"unclosed quotes" \
+		+ (err == ft_err_bad_access) * (intptr_t)"bad access rights" \
+		+ (err == ft_err_isadir) * (intptr_t)"is a directory" \
+		+ (err == ft_err_bad_syntax) * (intptr_t)"syntax error" \
+		+ (err == ft_err_bad_cmd) * (intptr_t)"command not found" \
+		+ (err == ft_err_bad_substitution) * (intptr_t)"bad substitution" \
+		));
+}
 
 t_ft_err   ft_err_perror(char const *msg, t_ft_err err)
 {
-    perror(msg);
+    ft_putstr_fd(msg, STDERR_FILENO);
+	ft_putstr_fd(ft_strerr(err), STDERR_FILENO);
+	ft_putendl_fd("", STDERR_FILENO);
     return (err);
 }
--- include/ft_error.h
+++ include/ft_error.h
@@ -7,11 +7,18 @@ typedef enum e_ft_err
 	ft_err_fail,
 	ft_err_bad_alloc,
 	ft_err_bad_arg,
-	ft_err_parse_error,
+	ft_err_toomanyargs,
+	ft_err_bad_syntax,
 	ft_err_overflow,
-	ft_err_user_define_error_namespace = (1 << 15)
+	ft_err_underflow,
+	ft_err_unclosedquotes,
+	ft_err_bad_access,
+	ft_err_bad_cmd,
+	ft_err_isadir,
+	ft_err_bad_substitution
 }			t_ft_err;
 
 t_ft_err	ft_err_perror(char const *msg, t_ft_err err);
+char const	*ft_strerr(t_ft_err err);
 
 #endif
