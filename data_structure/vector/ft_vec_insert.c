b66d091e2c0c53b661af4f7a3295d1962d543303
Function Diego <fdiego@vo-j1.msk.21-school.ru>
2021-10-11 19:30:30 +0300

    [vector] insert fix

--- data_structure/vector/ft_vec_insert.c
+++ data_structure/vector/ft_vec_insert.c
@@ -6,7 +6,7 @@
 /*   By: fdiego <fdiego@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 20:21:30 by fdiego            #+#    #+#             */
-/*   Updated: 2021/10/10 20:21:31 by fdiego           ###   ########.fr       */
+/*   Updated: 2021/10/11 19:04:44 by fdiego           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,12 @@ t_ft_err	ft_vec_insert(t_vector *vec, void *data, size_t pos)
 {
 	if (ft_vec_push_reserv(vec) != ft_err_ok)
 		return (ft_err_bad_alloc);
-	ft_memcpy(ft_vec_at(vec, pos + 1), ft_vec_at(vec, pos), \
+	ft_memmove(ft_vec_at(vec, pos + 1), ft_vec_at(vec, pos), \
 				vec->cell_size * (vec->size - pos));
 	if (data)
-		ft_memcpy(ft_vec_at(vec, vec->size), data, vec->cell_size);
+		ft_memcpy(ft_vec_at(vec, pos), data, vec->cell_size);
+	else
+		ft_bzero(ft_vec_at(vec, pos), vec->cell_size);
 	vec->size++;
 	return (ft_err_ok);
 }
